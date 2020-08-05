96e3548a3d497666f629eb94f38e0899fd7102d5
Gonéri Le Bouder <goneri@lebouder.net>
2020-08-04 23:24:54 -0400

    cloud-init: guess os version from the current kernel

--- plugins/package/cloud-init/tasks/77-cloud-init.sh
+++ plugins/package/cloud-init/tasks/77-cloud-init.sh
@@ -1,3 +1,5 @@
+OS_VERSION=$(sysctl -n kern.osrelease)
+
 git clone -b master http://github.com/goneri/cloud-init $TARGET/tmp/cloud-init
 
 echo 'nameserver 8.8.8.8' > $TARGET/etc/resolv.conf
@@ -16,10 +18,10 @@ pkgs="
    sudo--
 "
 for pkg in ${pkgs}; do
-    PKG_PATH="https://mirror.csclub.uwaterloo.ca/pub/OpenBSD/6.6/packages/amd64/" chroot $TARGET pkg_add ${pkg}
+    PKG_PATH="https://mirror.csclub.uwaterloo.ca/pub/OpenBSD/${OS_VERSION}/packages/amd64/" chroot $TARGET pkg_add ${pkg}
 done
 chroot $TARGET ldconfig /usr/local/lib
-PKG_PATH="https://mirror.csclub.uwaterloo.ca/pub/OpenBSD/6.6/packages/amd64/" chroot $TARGET sh -c 'cd /tmp/cloud-init; ./tools/build-on-openbsd' 
+PKG_PATH="https://mirror.csclub.uwaterloo.ca/pub/OpenBSD/${OS_VERSION}/packages/amd64/" chroot $TARGET sh -c 'cd /tmp/cloud-init; ./tools/build-on-openbsd'
 
 gsed -i "/^echo 'starting network'/i \/usr\/local\/bin\/cloud-init --debug init -l" $TARGET/etc/rc
 gsed -i "/^reorder_libs$/i \/usr\/local\/bin\/cloud-init --debug init" $TARGET/etc/rc
